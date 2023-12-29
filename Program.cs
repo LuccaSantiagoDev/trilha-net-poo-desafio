22bf1183d541b73933cb556d469b877e1bd5bf33
LuccaSantiagoDev <luccasantdev@gmail.com>
2023-12-29 18:38:14 -0300

    Net oop fundamentos Primeiro Commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone cel1 = new Nokia("4794899","65498494","Nokia 3",2048);
+cel1.InstalarAplicativo("Google Play");
+cel1.Ligar();
+cel1.ReceberLigacao();
+
+Console.WriteLine("==============================================");
+
+
+Smartphone cel2 = new Iphone("1415641","179889","Iphone 2",4096);
+cel2.InstalarAplicativo("App Store");
+cel2.Ligar();
+cel2.ReceberLigacao();
